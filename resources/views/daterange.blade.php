1b7f9cad168f9fdab642ae89ae332ddaad6b6e37
EdManDev <ed.manigat@gmail.com>
2020-10-21 13:45:45 -0400

    update

--- resources/views/daterange.blade.php
+++ resources/views/daterange.blade.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Laravel 7.28.4 - Daterange Filter in Datatables with Server-side Processing</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
@@ -21,23 +19,23 @@
 </head>
 <body>
   <div class="container">    
-     <br />
-     <h3 >Laravel 7.28.4 - Daterange Filter in Datatables with Server-side Processing</h3>
-     <br />
-            <br />
-            <div class="row input-daterange">
-                <div class="col-md-4">
-                    <input type="text" name="from_date" id="from_date" class="form-control" placeholder="From Date" readonly />
-                </div>
-                <div class="col-md-4">
-                    <input type="text" name="to_date" id="to_date" class="form-control" placeholder="To Date" readonly />
-                </div>
-                <div class="col-md-4">
-                    <button type="button" name="filter" id="filter" class="btn btn-primary">Search Date</button>
-                    <button type="button" name="refresh" id="refresh" class="btn btn-default">Reload</button>
-                </div>
-            </div>
-            <br />
+    <br />
+       <h3 >Laravel 7.28.4 - Daterange Filter in Datatables with Server-side Processing</h3>
+    <br />
+    <br />
+    <div class="row input-daterange">
+      <div class="col-md-4">
+        <input type="text" name="from_date" id="from_date" class="form-control" placeholder="From Date" readonly />
+      </div>
+      <div class="col-md-4">
+        <input type="text" name="to_date" id="to_date" class="form-control" placeholder="To Date" readonly />
+      </div>
+      <div class="col-md-4">
+        <button type="button" name="filter" id="filter" class="btn btn-primary">Search Date</button>
+        <button type="button" name="refresh" id="refresh" class="btn btn-default">Reload</button>
+      </div>
+    </div>
+  <br/>
    <div class="table-responsive">
     <table class="table table-bordered table-striped" id="order_table">
       <thead>
