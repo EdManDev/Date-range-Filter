772b8aec7e350322774b1680e96dcf9d256ba07c
EdManDev <ed.manigat@gmail.com>
2020-10-21 13:48:38 -0400

    title

--- resources/views/daterange.blade.php
+++ resources/views/daterange.blade.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <title>Laravel 7.28.4 - Daterange Filter in Datatables with Server-side Processing</title>
+  
+  <title>Daterange Filter</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
@@ -14,8 +14,6 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.js"></script>
 
-
-    <title>Document</title>
 </head>
 <body>
   <div class="container">    
